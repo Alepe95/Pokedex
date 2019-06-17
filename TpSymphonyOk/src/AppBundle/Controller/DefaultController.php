00c3fc36a0ec11cdff0c51c259a16e507181747f
Perez Alexis <perez_alexis@hotmail.fr>
2019-06-17 10:17:52 +0200

    Pokedex V7 - Symphony + Routes définis

--- TpSymphonyOk/src/AppBundle/Controller/DefaultController.php
+++ TpSymphonyOk/src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/liste", name="homepage")
      */
     public function indexAction(Request $request)
     {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $refPokemons = $em->getRepository('AppBundle:RefPokemon')->findAll();
+
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('refPokemon/index.html.twig', [
+            'refPokemons' => $refPokemons,
         ]);
     }
 }
